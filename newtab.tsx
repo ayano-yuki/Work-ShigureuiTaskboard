2c548195233366f2eb0066834890cbee86ffed73
ayano-yuki <ac234704@edu.okinawa-ct.ac.jp>
2025-04-22 20:56:22 +0900

    add: 新しいタブを開いたときの表示ページを変更できるようにした

---
+++ newtab.tsx
@@ -0,0 +1,13 @@
+// newtab.tsx
+import React from "react"
+
+function NewTab() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>🚀 新しいタブへようこそ！</h1>
+      <p>ここに好きなコンテンツを追加できます。</p>
+    </div>
+  )
+}
+
+export default NewTab
